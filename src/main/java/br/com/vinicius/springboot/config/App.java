2215da1fd45a74f20ba725ee757effc9bb38d56e
Vinicius Duarte <vinidgdon@gmail.com>
2018-09-22 19:20:39 -0300

    inclusao mysql

--- src/main/java/br/com/vinicius/springboot/config/App.java
+++ src/main/java/br/com/vinicius/springboot/config/App.java
@@ -1,31 +1,13 @@
 package br.com.vinicius.springboot.config;
 
-import java.text.ParseException;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages={"br.com.vinicius.springboot.controller"})
+@ComponentScan(basePackages = { "br.com.vinicius.springboot.controller" })
 public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
-	
-	@Value("${spring.jpa.hibernate.ddl-auto}")
-	private String strategy;
-	
-	@Bean
-	public boolean instantiateDatabase() throws ParseException {
-		
-		if (!"create".equals(strategy)) {
-			return false;
-		}
-		
-		return true;
-	}
-	
 }
