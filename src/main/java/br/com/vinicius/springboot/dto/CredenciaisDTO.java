7e9faaecdcda6756e96b0560651d130c3e445499
Vinicius Duarte Galdino <vduarteg@everis.com>
2019-01-21 16:29:05 -0200

    fix

--- src/main/java/br/com/vinicius/springboot/dto/CredenciaisDTO.java
+++ src/main/java/br/com/vinicius/springboot/dto/CredenciaisDTO.java
@@ -2,16 +2,29 @@ package br.com.vinicius.springboot.dto;
 
 import java.io.Serializable;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
 public class CredenciaisDTO implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 
 	private String user;
 	private String pass;
+	
+	public CredenciaisDTO() {
+	}
+	
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
+	
 	
 }
